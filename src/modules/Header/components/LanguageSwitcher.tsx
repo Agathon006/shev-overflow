499f83b30aee9626996c09cf0d7186f551dedbc7
Agathon006 <agaton615@gmail.com>
2025-02-03 13:50:36 +0300

    feat(i18n): i18n switcher

--- src/modules/Header/components/LanguageSwitcher.tsx
+++ src/modules/Header/components/LanguageSwitcher.tsx
@@ -1,9 +1,22 @@
 import LanguageIcon from '@mui/icons-material/Language';
 import IconButton from '@mui/material/IconButton';
+import { useTranslation } from 'react-i18next';
 
 export const LanguageSwitcher = () => {
+  const { i18n } = useTranslation();
+
+  const handleLanguageChange = () => {
+    const newLang = i18n.language === 'ru' ? 'en' : 'ru';
+    i18n.changeLanguage(newLang);
+  };
+
   return (
-    <IconButton aria-label="language" color="inherit" sx={{ ml: 1 }}>
+    <IconButton
+      aria-label="language"
+      color="inherit"
+      sx={{ ml: 1 }}
+      onClick={handleLanguageChange}
+    >
       <LanguageIcon fontSize="large" />
     </IconButton>
   );
