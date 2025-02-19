13ad7d18b7996e52f9ba8573aa131fba05b45c37
Agathon006 <agaton615@gmail.com>
2025-02-19 13:27:20 +0300

    refactor: snippetEditSchema partly made of snippetSchema now

--- src/modules/Snippets/schemas/snippetEdit.ts
+++ src/modules/Snippets/schemas/snippetEdit.ts
@@ -1,8 +1,10 @@
 import { z } from 'zod';
 
+import { snippetSchema } from '@/schemas/snippet';
+
 export const snippetEditSchema = z.object({
-  language: z.string().nonempty('Language is required'),
-  code: z.string().nonempty('Code snippet is required'),
+  language: snippetSchema.shape.language.nonempty('Language is required'),
+  code: snippetSchema.shape.code.nonempty('Code snippet is required'),
 });
 
 export type SnippetEditSchema = z.infer<typeof snippetEditSchema>;
